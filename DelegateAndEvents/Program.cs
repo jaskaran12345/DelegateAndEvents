dfa65a8de54c0acd0e1c9de4c50643d492680f51
visualstudio <visualstudio@230-PC19>
2019-03-07 16:37:25 -0500

    lab06

--- DelegateAndEvents/Program.cs
+++ DelegateAndEvents/Program.cs
@@ -21,19 +21,34 @@ namespace anmol
         {
             DelegateExercises delegateExercises = new DelegateExercises();
             delegateExercises.Method3();
+            Console.ReadLine();
         }
     }
     public class DelegateExercises
     {
         public delegate int MyDelegate();
-        void Method1()
+        int Method1(int intMethod1)
         {
-            System.Console.WriteLine("MyDelgate");
+            return intMethod1 * 2;
         }
+
+        int Method2(int intMethod2)
+        {
+            return intMethod2 * 10;
+        }
+
+        public void Method4(MyDelegate myDelegate)
+        {
+            int result = myDelegate(10);
+            Console.WriteLine(result);
+        }
+       
         public void Method3()
         {
             MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate();
+            Method4(myDelegate);
+            myDelegate = new MyDelegate(Method2);
+            Method4(myDelegate);
         }
     }
 }
