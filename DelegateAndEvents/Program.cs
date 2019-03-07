78caa419dc6cb5b5ed7bb7cba50bf7765a59a099
visualstudio <visualstudio@230-PC19>
2019-03-07 16:52:32 -0500

    lab09

--- DelegateAndEvents/Program.cs
+++ DelegateAndEvents/Program.cs
@@ -15,27 +15,11 @@ using System.Threading.Tasks;
 
 namespace anmol
 {
-    public class Program
-    {
-        public static void Main()
-        {
-            DelegateExercises delegateExercises = new DelegateExercises();
-            delegateExercises.Method3();
-            Console.ReadLine();
-        }
-    }public delegate void MyDelegate();
+    
+    public delegate void MyDelegate();
 
-    public class DelegateExercises
+    public class DelegateExercises : MyDelegate
     {
-        void Method1()
-        {
-            System.Console.WriteLine("Method1");
-        }
-        public void Method3()
-        {
-            MyDelegate myDelegate = new MyDelegate(Method1)
-            myDelegate();
-           
-        }
+      
     }
 }
